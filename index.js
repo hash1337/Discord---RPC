b884cf3766668eb36f8093f7da1755701f8038ae
hash <37172651+hash1337@users.noreply.github.com>
2018-03-23 22:46:11 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const DiscordRPC = require("discord-rpc");
+
+const ClientId = '426809152950632448';
+
+DiscordRPC.register(ClientId);
+
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+
+rpc.on('ready', () => {
+  console.log("ready");
+  rpc.setActivity({
+    details: `GET YOUR ASS BACK HERE!!!!`,
+    state: 'Put your hands all over myself',
+    largeImageKey: 'gach1',
+    largeImageText: 'Put your hands all over myself',
+    smallImageKey: 'gach2',
+    smallImageText: 'Boy next door',
+    instance: false,
+    startTimestamp: new Date(),
+    partySize: 1,
+    partyMax: 1,
+	matchSecret: 'slithers',
+	spectateSecret: 'sniff',
+    joinSecret: 'boop',
+  });
+});
+
+rpc.login(ClientId).catch(console.error);
